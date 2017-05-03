e9a9c931c4686d4279960a117a9299b8a40f554a
Ryoichi Hara <rychhr@gmail.com>
2017-05-03 20:06:42 +0900

    Change swiftlint rules

--- MyProject/ViewController.swift
+++ MyProject/ViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
 }
