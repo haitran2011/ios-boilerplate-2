91a5b3daff930c1bb7d0e5107b78cd212f29b285
Ryoichi Hara <rychhr@gmail.com>
2017-05-04 00:25:19 +0900

    Add ListItem protocol and Article class

---
+++ MyProject/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  MyProject
+//
+//  Created by Ryoichi Hara on 2017/05/04.
+//  Copyright © 2017年 Ryoichi Hara. All rights reserved.
+//
+
+import Foundation
+
+struct Article: ListItem {
+    
+    let title: String?
+}
---
+++ MyProject/ListItem.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  MyProject
+//
+//  Created by Ryoichi Hara on 2017/05/04.
+//  Copyright © 2017年 Ryoichi Hara. All rights reserved.
+//
+
+import Foundation
+
+protocol ListItem {
+    
+    var title: String? { get }
+}
