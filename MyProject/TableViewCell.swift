f1813489e8c3ef9f740756521b52976a500704f5
Ryoichi Hara <rychhr@gmail.com>
2017-05-03 22:33:36 +0900

    Add TableViewCell

---
+++ MyProject/TableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TableViewCell.swift
+//  MyProject
+//
+//  Created by Ryoichi Hara on 2017/05/03.
+//  Copyright © 2017年 Ryoichi Hara. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
