b19af66cf0beed3365d1de0e0ff9b3232b443c8f
Ryoichi Hara <rychhr@gmail.com>
2017-05-04 02:02:27 +0900

    Add Resource class

---
+++ MyProject/Resource.swift
@@ -0,0 +1,31 @@
+//
+//  Resource.swift
+//  MyProject
+//
+//  Created by Ryoichi Hara on 2017/05/04.
+//  Copyright © 2017年 Ryoichi Hara. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct Resource {
+    
+    static func jsonFrom(fileName: String?, bundle: Bundle = .main) -> JSON? {
+        var json: JSON? = nil
+        
+        guard let fileURL: URL = bundle
+            .url(forResource: fileName, withExtension: "json") else {
+            return nil
+        }
+        
+        do {
+            let data = try Data(contentsOf: fileURL)
+            json = JSON(data: data)
+        } catch {
+            debugPrint(error.localizedDescription)
+        }
+        
+        return json
+    }
+}
