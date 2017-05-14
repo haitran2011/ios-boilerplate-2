fdbbf40c671311cbea8314e182c674b2695e09c7
Ryoichi Hara <rychhr@gmail.com>
2017-05-14 14:09:13 +0900

    Add ListRepository

---
+++ MyProject/Domain/ListRepository.swift
@@ -0,0 +1,15 @@
+//
+//  ListRepository.swift
+//  MyProject
+//
+//  Created by Ryoichi Hara on 2017/05/14.
+//  Copyright © 2017年 Ryoichi Hara. All rights reserved.
+//
+
+import Foundation
+
+protocol ListRepositoryInput {
+}
+
+struct ListRepository: ListRepositoryInput {
+}
