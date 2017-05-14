5049db74fbf177b4bf964a646c9921c2a91da924
Ryoichi Hara <rychhr@gmail.com>
2017-05-14 14:23:59 +0900

    Add ListUseCase

---
+++ MyProject/Domain/ListUseCase.swift
@@ -0,0 +1,18 @@
+//
+//  ListUseCase.swift
+//  MyProject
+//
+//  Created by Ryoichi Hara on 2017/05/14.
+//  Copyright © 2017年 Ryoichi Hara. All rights reserved.
+//
+
+import Foundation
+
+protocol ListUseCaseInput {
+}
+
+protocol ListUseCaseOutput {
+}
+
+struct ListUseCase {
+}
