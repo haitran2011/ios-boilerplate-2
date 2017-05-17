e184888398aa39efb584c84a8030d6dbb02710dc
Ryoichi Hara <rychhr@gmail.com>
2017-05-18 01:08:13 +0900

    Add DetailViewController

---
+++ MyProject/Presentation/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  MyProject
+//
+//  Created by Ryoichi Hara on 2017/05/18.
+//  Copyright © 2017年 Ryoichi Hara. All rights reserved.
+//
+
+import UIKit
+
+final class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
