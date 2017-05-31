12ec336c8db64309b66651381aba2d10d9a21257
moaible <motodera@moapp.info>
2017-05-31 09:33:27 +0900

    Add RealmClient.Request

--- Data/RealmClient/RealmClient.swift
+++ Data/RealmClient/RealmClient.swift
@@ -6,6 +6,45 @@
 //  Copyright © 2017年 Ryoichi Hara. All rights reserved.
 //
 
-class RealmClient {
+import RealmSwift
 
+struct RealmClient {
+    
+    fileprivate static var source: [String: Realm] = [:]
+    
+    fileprivate init() {}
+}
+
+enum RealmBaseDirectory {
+    
+    /// 'Documents/' バックアップ対象となりユーザーから見える
+    case documents
+    
+    /// 'Documents/Inbox/' バックアップ対象となりユーザーから見える、他のアプリと共有できる
+    case sharedAppDocuments
+    
+    /// 'Library/Caches/' バックアップ対象外でユーザーから見えない
+    case libraryCaches
+    
+    case inMemory
+    case custom(baseDirectory: String)
+}
+
+protocol RealmConfigration {
+    
+    var baseDirectory: RealmBaseDirectory { get }
+    
+    var path: String { get }
+    
+    var encryptionKey: Data? { get }
+    
+    var readOnly: Bool { get }
+    
+    var schemaVersion: UInt64 { get }
+    
+    var migrationBlock: RealmSwift.MigrationBlock? { get }
+    
+    var deleteRealmIfMigrationNeeded: Bool { get }
+    
+    var objectTypes: [Object.Type]? { get }
 }
